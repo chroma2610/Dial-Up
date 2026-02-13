618079d589c37ab1ee85833e5679cf2a357681cb
Shiloh Harrison <shilohharrison2610@gmail.com>
2026-02-13 14:31:49 -0800

    did cool things with the lighting

--- player.gd
+++ player.gd
@@ -1,17 +1,22 @@
 extends CharacterBody3D
 
 @onready var sprite: AnimatedSprite3D = $sprite
+@onready var interaction_cast: ShapeCast3D = $InteractionCast
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const GROUND_FRICTION := 0.7
 
+func _unhandled_key_input(event: InputEvent) -> void:
+    if event.is_action_pressed("interact"):
+        if interaction_cast.is_colliding():
+            print(interaction_cast.get_collider(0))
 
 func _physics_process(delta: float) -> void:
     
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
-    var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    var input_dir := Input.get_vector("left", "right", "up", "down")
     var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
     
     handle_ground_movement(direction)
@@ -19,6 +24,7 @@ func _physics_process(delta: float) -> void:
     
 func handle_ground_movement(direction):
     if direction:
+        interaction_cast.target_position = direction
         if direction.x > 0: sprite.flip_h = true
         elif direction.x < 0: sprite.flip_h = false
         sprite.play("walk")
