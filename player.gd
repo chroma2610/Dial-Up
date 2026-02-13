583554a0b524027668415a2661a76d02d5a1fae7
Shiloh Harrison <shilohharrison2610@gmail.com>
2026-02-13 13:41:20 -0800

    added a bunch of art

--- player.gd
+++ player.gd
@@ -1,28 +1,32 @@
 extends CharacterBody3D
 
+@onready var sprite: AnimatedSprite3D = $sprite
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const GROUND_FRICTION := 0.7
 
 
 func _physics_process(delta: float) -> void:
-    # Add the gravity.
-    if not is_on_floor():
-        velocity += get_gravity() * delta
-
-    # Handle jump.
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
-
+    
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
     var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
     var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+    
+    handle_ground_movement(direction)
+    move_and_slide()
+    
+func handle_ground_movement(direction):
     if direction:
+        if direction.x > 0: sprite.flip_h = true
+        elif direction.x < 0: sprite.flip_h = false
+        sprite.play("walk")
         velocity.x = direction.x * SPEED
         velocity.z = direction.z * SPEED
     else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.z = move_toward(velocity.z, 0, SPEED)
-
-    move_and_slide()
+        sprite.play("idle")
+        var v = Vector2(velocity.x, velocity.z)
+        v = v.move_toward(Vector2.ZERO, GROUND_FRICTION)
+        velocity.x = v.x
+        velocity.z = v.y
